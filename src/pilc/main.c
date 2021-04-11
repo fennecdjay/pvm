7f5f363400d3d226a5de67bf57817f4d702a681a
apachejuice <ubuntugeek1904@gmail.com>
2021-04-11 16:42:47 +0300

    Removed pilc

--- src/pilc/main.c
+++
@@ -1,9 +0,0 @@
-// main.c: entry point to pilc
-// license information in LICENSE
-#include <stdio.h>
-
-int libpilc_main (int argc, char* argv[])
-{
-    printf ("Hello pilc!\n");
-    return 0;
-}
--- src/pilc/main.h
+++
@@ -1,8 +0,0 @@
-// main.h: entry point to pilc
-// license information in LICENSE
-#ifndef __PILC_MAIN_H__
-#define __PILC_MAIN_H__
-
-int libpilc_main (int argc, char* argv[]);
-
-#endif /* __PILC_MAIN_H__ */
